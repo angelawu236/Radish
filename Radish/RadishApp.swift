750163b686449e76e1173d1c380113b29318ef12
angelawu236 <anw041@ucsd.edu>
2025-07-12 00:28:53 -0700

    initial committ

---
+++ Radish/RadishApp.swift
@@ -0,0 +1,17 @@
+//
+//  RadishApp.swift
+//  Radish
+//
+//  Created by Angela Wu on 7/12/25.
+//
+
+import SwiftUI
+
+@main
+struct RadishApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
